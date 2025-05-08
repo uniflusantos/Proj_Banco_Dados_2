dfe624c94c2f457ad3e0e85462b0ce7209a61613
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-05-08 13:38:23 -0300

    Update query_2.sql

--- queries/query_2.sql
+++ queries/query_2.sql
@@ -1,3 +1,4 @@
+--Query: Liste todos os chefes de departamento e coordenadores de curso em apenas uma query de forma que a primeira coluna seja o nome do professor, a segunda o nome do departamento coordenado e a terceira o nome do curso que coordena. Substitua os campos em branco do resultado da query pelo texto "nenhum".
 SELECT
 	p."Nome" AS Nome_Professor,
 	COALESCE(d."Nome", 'nenhum') AS Departamento_Chefiado,
@@ -5,4 +6,7 @@ SELECT
 FROM
 	"Professor" p
 	LEFT JOIN "Departamento" d ON p."Prof_ID" = d."Chefe"
-	LEFT JOIN "Curso" c ON p."Prof_ID" = c."Coordenador";
+	LEFT JOIN "Curso" c ON p."Prof_ID" = c."Coordenador"
+WHERE
+	d."Nome" IS NOT NULL
+	OR c."Instrumento" IS NOT NULL;
