415ece650a2534b61c22fa80ce24effc3ab50ef0
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-05-08 19:28:20 -0300

    Update query_4.sql

--- queries/query_4.sql
+++ queries/query_4.sql
@@ -1,3 +1,14 @@
+--Liste os IDs dos professores que ensinam mais de uma disciplina.
+SELECT
+	hp."Prof_ID"
+FROM
+	"Historico_Professor" hp
+GROUP BY
+	hp."Prof_ID"
+HAVING
+	COUNT(DISTINCT hp."Disc_ID") >= 2
+ORDER BY
+	hp."Prof_ID";
 SELECT
 	hp."Prof_ID"
 FROM
