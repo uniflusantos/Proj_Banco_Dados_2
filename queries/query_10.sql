11fe1c04377fb4ad612cf0d09b00e4d8bf8a36c5
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-05-08 13:46:22 -0300

    Update query_10.sql

--- queries/query_10.sql
+++ queries/query_10.sql
@@ -1,3 +1,4 @@
+--Um professor da escola de música quer montar uma banda com alguns dos alunos da escola. Ele quer alunos que cursaram as disciplinas de guitarra, baixo, bateria e canto. Mostre os nomes dos alunos que cursaram essas disciplinas, além de qual das disciplinas eles cursaram. Ordene pelo nível da disciplina.
 SELECT
 	a."Nome" AS Nome_Aluno,
 	d."Instrumento" AS Disciplina_Cursada,
