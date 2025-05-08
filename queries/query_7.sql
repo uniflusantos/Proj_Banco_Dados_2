dad35892e42a0ededb39b2e31daed5c030ea9c43
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-05-08 19:30:18 -0300

    Update query_7.sql

--- queries/query_7.sql
+++ queries/query_7.sql
@@ -1,8 +1,10 @@
+--Liste os cursos que foram ministrados pelos professores de ID"PP0800" e "PO9444".
 SELECT DISTINCT
-	c."Instrumento" AS Nome_Curso
+	hp."Prof_ID", -- Adicionado ID do professor
+	d."Instrumento" AS Nome_Disciplina,
+	d."Nivel"
 FROM
-	"Curso" c
-	JOIN "Matriz_Curricular" mc ON c."Curso_ID" = mc."Curso"
-	JOIN "Historico_Professor" hp ON mc."Disciplina" = hp."Disc_ID"
+	"Disciplina" d
+	JOIN "Historico_Professor" hp ON d."Disc_ID" = hp."Disc_ID"
 WHERE
 	hp."Prof_ID" IN ('PP0800', 'PO9444');
