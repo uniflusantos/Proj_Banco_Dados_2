493cd7f3d3899d20c0265a2d75ee51711a8d4038
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-05-09 14:39:43 -0300

    Update query_11.sql

--- queries/query_11.sql
+++ queries/query_11.sql
@@ -8,7 +8,7 @@ FROM
 	JOIN "Historico_Aluno" ha ON a."RA" = ha."RA"
 	JOIN "Disciplina" d ON ha."Disc_ID" = d."Disc_ID"
 WHERE
-	d."Instrumento" IN ('Violão', 'Guitarra', 'Canto')
+	d."Instrumento" IN ('Violao', 'Guitarra', 'Canto')
 	AND a."RA" IN (
 		SELECT DISTINCT
 			ha_inner."RA"
@@ -25,5 +25,5 @@ WHERE
 			"Historico_Aluno" ha_inner
 			JOIN "Disciplina" d_inner ON ha_inner."Disc_ID" = d_inner."Disc_ID"
 		WHERE
-			d_inner."Instrumento" IN ('Violão', 'Guitarra')
+			d_inner."Instrumento" IN ('Violao', 'Guitarra')
 	);
